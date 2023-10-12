8237e9b519160b3cc3a900eafa0ed55e12a7b3ba
Yaleruby <ethynnguyen@yahoo.com>
2023-10-12 10:41:32 -0500

    Added ProfileActivity and EditAccountActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui:2.7.2")
     implementation("com.google.firebase:firebase-auth:22.1.1")
     implementation("com.google.android.gms:auth-api-impl:11.6.0")
+    implementation("com.google.firebase:firebase-firestore:24.8.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/crittercalls/RegisterActivity.java
+++ app/src/main/java/com/example/crittercalls/RegisterActivity.java
@@ -1,22 +1,38 @@
 package com.example.crittercalls;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.*;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText firstName, lastName, userEmail, userPassword, confirmPassword;
+    private String userID;
     private Button regBtn;
+    private ProgressBar registerProgressBar;
     private TextView loginLink;
     private Intent homeIntent;
     private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +45,14 @@ public class RegisterActivity extends AppCompatActivity {
         userPassword = findViewById(R.id.reg_password);
         confirmPassword = findViewById(R.id.reg_confirm_password);
         regBtn = findViewById(R.id.btn_reg);
+        registerProgressBar = findViewById(R.id.reg_progress);
 
         loginLink = findViewById(R.id.text_login);
 
         homeIntent = new Intent(this, MainActivity.class);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
 
         addListeners();
     }
@@ -47,11 +65,18 @@ public class RegisterActivity extends AppCompatActivity {
             String password = userPassword.getText().toString();
             String confirmPass = confirmPassword.getText().toString();
 
+            regBtn.setVisibility(View.INVISIBLE);
+            registerProgressBar.setVisibility(View.VISIBLE);
+
             if(first.isEmpty() || last.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPass.isEmpty()) {
-                 showMessage("Invalid Inputs!");
+                regBtn.setVisibility(View.VISIBLE);
+                registerProgressBar.setVisibility(View.INVISIBLE);
+                showMessage("Invalid Inputs!");
             }
             else if (!password.equals(confirmPass))
             {
+                regBtn.setVisibility(View.VISIBLE);
+                registerProgressBar.setVisibility(View.INVISIBLE);
                 showMessage("Password does not match Confirm Password");
             }
             else {
@@ -74,12 +99,34 @@ public class RegisterActivity extends AppCompatActivity {
         firebaseAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(task -> {
                     if(task.isSuccessful()) {
+                        userID = firebaseAuth.getCurrentUser().getUid();
+                        DocumentReference documentReference = firestore.collection("users").document(userID);
+                        Map<String, Object> user = new HashMap<>();
+                        user.put("firstName", first);
+                        user.put("lastName", last);
+                        user.put("email", email);
+                        user.put("password", password);
+
+                        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Log.d(TAG, "onSuccess: user Profile is created for " + userID);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d(TAG, "onFailure: " + e.toString());
+                            }
+                        });
+
                         startActivity(homeIntent);
                         finish();
                     }
                     else {
-//                        registerUser(name, email, password);
-                        showMessage((task.getException().getMessage()));
+                        String errorMsg = task.getException().getMessage();
+                        regBtn.setVisibility(View.VISIBLE);
+                        registerProgressBar.setVisibility(View.INVISIBLE);
+                        showMessage(errorMsg);
                     }
                 });
     }
