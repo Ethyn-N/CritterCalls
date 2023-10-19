7d8c81a80676a5fcee1f88d4bb88fd2cc760c0ce
hunter <hxb1638@mavs.uta.edu>
2023-10-19 10:24:49 -0500

    hb initial commit

--- app/src/main/java/com/example/crittercalls/UserProfile.java
+++ app/src/main/java/com/example/crittercalls/UserProfile.java
@@ -4,48 +4,58 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class UserProfile extends AppCompatActivity {
 
-    private String fname;
-    private String lname;
-    private String email;
-    private Image photo;
-    private int id;
-    private String profession;
+    private TextView name, email, profession, UTA_id;
+    private ImageView photo;
+    private Button logout_button, edit_acc_button;
     private FirebaseAuth auth;
-
-    public UserProfile(String fname, String lname, String email, Image photo, int id, String profession)
-    {
-        this.fname = fname;
-        this.lname = lname;
-        this.email = email;
-        this.photo = photo;
-        this.id = id;
-        this.profession = profession;
-    }
-
-    public UserProfile(String fname, String lname)
-    {
-        this.fname = fname;
-        this.lname = lname;
-    }
-
-    private void logout()
-    {
-        auth.signOut();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-        //this.email = findViewById(R.id.login_email);
+        email = findViewById(R.id.profile_email);
+        name = findViewById(R.id.profile_name);
+        UTA_id = findViewById(R.id.profile_UTA_ID);
+        profession = findViewById(R.id.profile_profession);
+        photo = findViewById(R.id.profile_img);
+
+        logout_button = findViewById(R.id.logout_btn);
+        edit_acc_button = findViewById(R.id.profile_edit_account_link);
+
+        auth = FirebaseAuth.getInstance();
+
+        addListeners();
     }
 
+    private void addListeners()
+    {
+        logout_button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
 
+            }
+        }
+        );
+    }
+
+    private void logout()
+    {
+        auth.signOut();
+    }
 
 }
